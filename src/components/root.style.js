3da6f8a2d165e9e0ed3d55c1fe99ee3441f08bb8
tusooa <tusooa@kazv.moe>
2024-05-31 09:38:42 -0400

    Ensure selection text color has enough contrast

--- src/components/root.style.js
+++ src/components/root.style.js
@@ -37,7 +37,7 @@ export default {
 
         // Selection colors
         '--selectionBackground': 'color | --accent',
-        '--selectionText': 'color | $textColor(--accent, --text)'
+        '--selectionText': 'color | $textColor(--accent, --text, no-preserve)'
       }
     }
   ]
