2c7f78a29fd040a655c74951c2d1919e736ce133
Henry Jameson <me@hjkos.com>
2024-07-22 01:19:05 +0300

    fix tests

--- test/unit/specs/services/theme_data/theme_data3.spec.js
+++ test/unit/specs/services/theme_data/theme_data3.spec.js
@@ -66,7 +66,7 @@ describe('Theme Data 3', () => {
     this.timeout(5000)
 
     it('Test initialization without anything', () => {
-      const out = init({ ruleset: [], ultimateBackgroundColor: '#DEADAF' })
+      const out = init({ inputRuleset: [], ultimateBackgroundColor: '#DEADAF' })
 
       expect(out).to.have.property('eager')
       expect(out).to.have.property('lazy')
@@ -86,7 +86,7 @@ describe('Theme Data 3', () => {
 
     it('Test initialization with a basic palette', () => {
       const out = init({
-        ruleset: [{
+        inputRuleset: [{
           component: 'Root',
           directives: {
             '--bg': 'color | #008080',
@@ -109,7 +109,7 @@ describe('Theme Data 3', () => {
 
     it('Test initialization with opacity', () => {
       const out = init({
-        ruleset: [{
+        inputRuleset: [{
           component: 'Root',
           directives: {
             '--bg': 'color | #008080'
