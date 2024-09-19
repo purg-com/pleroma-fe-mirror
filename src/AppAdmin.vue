e57a11f3b1a9be3abf34db47e56d578d514f71ed
Henry Jameson <me@hjkos.com>
2024-09-19 14:43:32 +0300

    styling for admin page

--- src/AppAdmin.vue
+++ src/AppAdmin.vue
@@ -1,8 +1,10 @@
 <template>
-  <div id="app-loaded" v-if="currentUser">
+  <div id="app-loaded" v-if="currentUser" class="settings-modal">
     <SettingsModalAdminContent />
   </div>
 </template>
 
 <script src="./AppAdmin.js"></script>
 <style lang="scss" src="./App.scss"></style>
+<style lang="scss" src="src/components/settings_modal/settings_modal.scss"></style>
+<style lang="scss" src="./AppAdmin.scss"></style>
