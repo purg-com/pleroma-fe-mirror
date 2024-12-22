50b1c5b640a5867cd0f988276ffb6946038a96ea
Henry Jameson <me@hjkos.com>
2024-12-22 16:49:42 +0200

    fix theme3hacks resetting theme to default-v2

--- src/modules/config.js
+++ src/modules/config.js
@@ -307,7 +307,7 @@ const config = {
           applyConfig(state)
         }
         if (name.startsWith('theme3hacks')) {
-          dispatch('setTheme', { recompile: true })
+          dispatch('applyTheme', { recompile: true })
         }
         switch (name) {
           case 'theme':
