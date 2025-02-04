bb2de0f56789f19be92dda03c458d9573832b092
Henry Jameson <me@hjkos.com>
2025-02-04 14:13:26 +0200

    migrate to eslint 9

---
+++ eslint.config.mjs
@@ -0,0 +1,37 @@
+import vue from "eslint-plugin-vue";
+import js from "@eslint/js";
+import globals from "globals";
+
+
+export default [
+  ...vue.configs['flat/recommended'],
+  js.configs.recommended,
+  {
+    files: ["**/*.js", "**/*.mjs", "**/*.vue"],
+    ignores: ["build/*.js", "config/*.js"],
+
+    languageOptions: {
+      ecmaVersion: 2024,
+      sourceType: "module",
+
+      parserOptions: {
+        parser: "@babel/eslint-parser",
+      },
+      globals: {
+        ...globals.browser,
+        ...globals.mocha,
+        ...globals.chai,
+        ...globals.commonjs,
+        ...globals.serviceworker
+      }
+    },
+
+    rules: {
+      'arrow-parens': 0,
+      'generator-star-spacing': 0,
+      'no-debugger': 0,
+      'vue/require-prop-types': 0,
+      'vue/multi-word-component-names': 0,
+    }
+  }
+]
