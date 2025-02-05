21af30f71b9500781679c164a3b3b3b751928d34
Henry Jameson <me@hjkos.com>
2025-02-05 08:07:48 +0200

    store cache eagerly

--- src/services/style_setter/style_setter.js
+++ src/services/style_setter/style_setter.js
@@ -140,6 +140,7 @@ export const applyTheme = (
     }
   }
 
+  let cache
   const { lazyProcessFunc } = generateTheme(
     input,
     {
@@ -152,16 +153,13 @@ export const applyTheme = (
       },
       onEagerFinished () {
         adoptStyleSheets([eagerStyles])
+        cache = { engineChecksum: getEngineChecksum(), data: [eagerStyles.rules, lazyStyles.rules] }
+        localforage.setItem('pleromafe-theme-cache', cache)
         onEagerFinish()
       },
       onLazyFinished () {
         adoptStyleSheets([eagerStyles, lazyStyles])
-        const cache = { engineChecksum: getEngineChecksum(), data: [eagerStyles.rules, lazyStyles.rules] }
         onFinish(cache)
-        const compress = (js) => {
-          return pako.deflate(JSON.stringify(js))
-        }
-        localforage.setItem('pleromafe-theme-cache', compress(cache))
       }
     },
     debug
